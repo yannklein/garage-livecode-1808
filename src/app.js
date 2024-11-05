1f54ce38f3859ddd25ad003a4982d17515f75be4
Yann Klein <yann.klein@me.com>
2024-11-05 17:59:30 +0900

    livecode done

--- src/app.js
+++ src/app.js
@@ -1,70 +1,90 @@
 // DON'T CHANGE THIS LINE
-window.myBadAssGarage = 'drift-container';
+window.myBadAssGarage = 'fhat-rocket';
 if (myBadAssGarage)
   document.querySelector('#garage-name').innerText = myBadAssGarage.replace(
     /-/g,
     ' ',
   );
 
-// //////////////////////
-// PSEUDOCODE
-// //////////////////////
-const url = 'https://wagon-garage-api.herokuapp.com/drift-container/cars';
+
+const displayCars = (cars) => {
+  carGarage.innerHTML = "";
+  cars.forEach((car) => {
+    const owner = car.owner;
+    const plate = car.plate;
+    const model = car.model;
+    const brand = car.brand;
+    // insert into list
+    carGarage.insertAdjacentHTML(
+      "beforeend", 
+      `<div class="car">
+        <div class="car-image">
+          <img src="http://loremflickr.com/280/280/${brand},${model}" />
+        </div>
+        <div class="car-info">
+          <h4>${brand} ${model}</h4>
+          <p><strong>Owner:</strong> ${owner}</p>
+          <p><strong>Plate:</strong> ${plate}</p>
+        </div>
+      </div>`
+    );
+  });
+};
+
+const addCar = (event) => {
+  const buildRadCar = () => {
+    return {
+      owner: ownerInput.value,
+      brand: brandInput.value,
+      plate: plateInput.value,
+      model: modelInput.value
+    }
+  }
+  
+  event.preventDefault();
+  fetch(garageUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(buildRadCar())
+  }).then(response => response.json())
+  .then((data) => {
+    console.log(data);
+    refresh();
+  })
+}
 
 // Get all the cars
 // //////////////////////
-const getCars = () => {
-  // Select class cars-list
-  const cars = document.querySelector('.cars-list');
-  // fetch GET api returns an array
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      // iterate over the array and for each car
-      cars.innerHTML = '';
-      data.forEach((car) => {
-        const carDesc = `
-      <div class="car">
-        <div class="car-image">
-          <img src="http://loremflickr.com/280/280/${car.brand},${car.model}" />
-        </div>
-        <div class="car-info">
-          <h4>${car.brand} ${car.model}</h4>
-          <p><strong>Owner:</strong> ${car.owner}</p>
-          <p><strong>Plate:</strong> ${car.plate}</p>
-        </div>
-      </div>`;
-        // Insert the car into the cars-list
-        cars.insertAdjacentHTML('afterbegin', carDesc);
-      });
-    });
+
+// select cars-list
+const carGarage = document.querySelector(".cars-list");
+// fetch api get
+const garageUrl = "https://garage.api.lewagon.com/fhat-rocket/cars"
+const refresh = () => {fetch(garageUrl) 
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data);
+    // iterate through the cars
+    displayCars(data);
+  });
 };
 
-getCars();
+refresh();
 
 // Add a new car
 // //////////////////////
-const addCar = document.querySelector('#submit-btn');
 
-// add event listener(submit)
-addCar.addEventListener("click", (event) => {
-  event.preventDefault();
-  // select the inputs from the user
-  const brand = document.querySelector('#brand').value;
-  const model = document.querySelector('#model').value;
-  const plate = document.querySelector('#plate').value;
-  const owner = document.querySelector('#owner').value;
-  const data = { brand, model, plate, owner };
-  // fetch post the user inputs
-  fetch(url, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data)
-  })
-    .then((response) => response.json())
-    .then(data => {
-      console.log(data);
-      // refresh the cars-list
-      getCars();
-    });
-})
+// select inputs (four)
+const brandInput = document.querySelector("#brand");
+const modelInput = document.querySelector("#model");
+const plateInput = document.querySelector("#plate");
+const ownerInput = document.querySelector("#owner");
+// select form 
+const form = document.querySelector(".car-form");
+// listen to a submit on the form
+
+form.addEventListener("submit", addCar)
+// fetch api post
+// get all the cars again
